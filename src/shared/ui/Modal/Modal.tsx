6f2bab747fe05a8595fc9eb0aaabecb3996c1064
Lugertr <nikitavishnev@bk.ru>
2026-01-07 09:35:12 +0300

    add modal window

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,47 @@
+import { type Mods, classNames } from '@/shared/lib/classNames/classNames';
+import { useModal } from '@/shared/lib/hooks/useModal/useModal';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
+import { Overlay } from '@/shared/Overlay';
+import type { ReactNode } from 'react';
+import { Portal } from '../Portal/Portal';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+    lazy?: boolean;
+}
+
+const ANIMATION_DELAY = 300;
+
+export const Modal = (props: ModalProps) => {
+    const { className, children, isOpen, onClose, lazy } = props;
+
+    const { close, isClosing } = useModal({
+        animationDelay: ANIMATION_DELAY,
+        onClose,
+        isOpen,
+    });
+
+    const { theme } = useTheme();
+
+    const mods: Mods = {
+        [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing,
+    };
+
+    if (lazy && !isOpen) {
+        return null;
+    }
+
+    return (
+        <Portal element={document.getElementById('app') ?? document.body}>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
+                <Overlay onClick={close} />
+                <div className={cls.content}>{children}</div>
+            </div>
+        </Portal>
+    );
+};
--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,6 +5,7 @@ export default {
     rules: {
         'declaration-no-important': true,
         'selector-class-pattern': null,
-        'no-descending-specificity': null
+        'no-descending-specificity': null,
+        'order/properties-order': null
     },
 };
